10825b29adb807f8b53576b0c05fb8735211e29b
Antonio Silva <antoniobsilvaprof@hotmail.com>
2023-06-06 17:13:29 -0300

    feat: install fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular, DMSerifDisplay_400Regular
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
